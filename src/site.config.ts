0a5b573357b892c7c24de71dffa6c97eadc756b6
Isaac Almanza <isaac.almanza19@gmail.com>
2024-01-02 21:53:14 -0500

    feat: update css

--- src/site.config.ts
+++ src/site.config.ts
@@ -28,15 +28,7 @@ export const siteConfig: SiteConfig = {
 // Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
 	{
-		title: "Home",
-		path: "/",
-	},
-	{
 		title: "About",
 		path: "/about/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
 	},
 ];
